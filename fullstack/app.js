1278e6e03a6b8b72cf2bf2f16ca5329d959d47ae
aakashdeveloper <ahanda205@gmail.com>
2023-10-18 21:01:27 +0530

    menu

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,12 +1,27 @@
 let express = require('express');
 let app = express();
 let port = 9101;
-let categoryRouter = require('./src/controller/categoryRouter');
-let productRouter = require('./src/controller/productsRouter');
+
+let menu = [
+    {link:"/",name:"Home"},
+    {link:"/category",name:"Category"},
+    {link:"/products",name:"Products"}
+]
+
+let categoryRouter = require('./src/controller/categoryRouter')(menu);
+let productRouter = require('./src/controller/productsRouter')(menu);
+
+//static file path
+app.use(express.static(__dirname+'/public'))
+// ejs file path
+app.set('views','./src/views')
+// view engine name
+app.set('view engine','ejs')
 
 //default Route
 app.get('/',function(req,res){
-    res.send('Hiii From Express');
+    //res.send('Hiii From Express');
+    res.render('index',{title:'Home Page',menu})
 });
 
 app.use('/category',categoryRouter);
--- fullstack/src/controller/categoryRouter.js
+++ fullstack/src/controller/categoryRouter.js
@@ -1,14 +1,45 @@
 let express = require('express');
 let categoryRouter = express.Router();
 
-categoryRouter.route('/')
+let Category = [
+    {
+        "id":1,
+        "category": "Fashion",
+        "thumb":"https://i.ibb.co/56VP0Fn/cloths.jpg"
+    },
+    {
+        "id":2,
+        "category":"Electronics",
+        "thumb":"https://i.ibb.co/pw5Wtdx/appliances.jpg"
+    },
+    {
+        "id":3,
+        "category":"Essentials",
+        "thumb":"https://i.ibb.co/0cw34xm/essentials.jpg"
+    },
+    {
+        "id":4,
+        "category": "Footwear",
+        "thumb":"https://i.ibb.co/r3SZq8S/footware.jpg"
+    }
+]
+
+function router(menu){
+    
+    categoryRouter.route('/')
     .get((req,res) => {
-        res.send("Express Category Route")
+        //res.send(Category)
+        res.render('category',{title:'Category Page',data:Category,menu})
     })
 
-categoryRouter.route('/details')
+    categoryRouter.route('/details')
     .get((req,res) => {
         res.send("Hii From Category Details Route");
     })
 
-module.exports = categoryRouter;
+    return categoryRouter
+}
+
+
+
+module.exports = router;
