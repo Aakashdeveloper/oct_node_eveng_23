48fa68350a76f6a89e5f372fbca4273096fe76f8
aakashdeveloper <ahanda205@gmail.com>
2023-10-27 20:50:14 +0530

    apicallingg

---
+++ weather/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+//let request = require('request')
+import request from 'request';
+let port = 7660;
+let app = express();
+
+app.use(express.static(__dirname+'/public'));
+app.set('views','./src/views');
+app.set('view engine','ejs')
+
+app.get('/weather',(req,res)=>{
+    let city = req.query.city?req.query.city:'Delhi'
+    let url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+    //api calling
+    request(url,(err,response) => {
+        if(err) throw err;
+        const output = JSON.parse(response.body)
+        //res.send(output)
+        res.render('index',{title:'Weather App', result:output})
+    })
+})
+
+app.listen(port,(err) => {
+    if(err) throw err;
+    console.log(`Running on port ${port}`)
+})
